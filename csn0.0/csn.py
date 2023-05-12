3e4398917fa46f0bdc4cf434f45b21c60b77675f
bennlol <52867429+bennlol@users.noreply.github.com>
2023-05-11 21:50:28 -0500

    Update csn.py

--- csn0.0/csn.py
+++ csn0.0/csn.py
@@ -59,12 +59,12 @@ model = Sequential()
 
 model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(800,800,1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
 
 model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
 # model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
 # model.add(MaxPooling2D(pool_size=(2, 2)))

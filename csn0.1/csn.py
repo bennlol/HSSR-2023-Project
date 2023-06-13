a017a1040a0125247ec6399fcc027e952d36488d
Benjamin Namikas <52867429+bennlol@users.noreply.github.com>
2023-06-13 17:15:38 -0500

    Changed to match version run on Collab

--- csn0.1/csn.py
+++ csn0.1/csn.py
@@ -61,11 +61,11 @@ model.add(Dropout(0.2))
 
 model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.1))
+
+# model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.1))
 
 # model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
 # model.add(MaxPooling2D(pool_size=(2, 2)))

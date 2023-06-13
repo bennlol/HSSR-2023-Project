0413c1fa6f5f17be73a36e688420ca157a4feb0a
Benjamin Namikas <52867429+bennlol@users.noreply.github.com>
2023-06-13 17:09:29 -0500

    downscaled the model

--- csn0.2/csn.py
+++ csn0.2/csn.py
@@ -62,16 +62,14 @@ model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
-
-# model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
+# model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
 # model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.2))
+# model.add(Dropout(0.1))
+
+
 
 model.add(Flatten())
-# model.add(BatchNormalization())
+model.add(BatchNormalization())
 model.add(Dense(64, activation='relu'))
 
 input1=Input((800,800,1))

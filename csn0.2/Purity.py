bbec1c6ec6d787c63c638459cccd91e2f3c09bd9
Benjamin Namikas <52867429+bennlol@users.noreply.github.com>
2023-06-13 16:37:05 -0500

    Update Purity.py

--- csn0.2/Purity.py
+++ csn0.2/Purity.py
@@ -1,8 +1,16 @@
 # take pairs of images from a cluseter true image predicted value and decision
 import cv2, os, random, sys
 import numpy as np
+from keras.models import Sequential, model_from_json
 
 CLUSTER = 1
+
+with open('model.json', 'r') as json_file:
+    siamese_json = json_file.read()
+    
+siamese = model_from_json(siamese_json)
+siamese.load_weights("model.h5")
+
 
 def load_data(cluster):
     DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
@@ -20,4 +28,10 @@ def load_data(cluster):
 
 paths = load_data(CLUSTER)
 
-for i in 
+for i in range(len(paths)):
+    path1 = paths[i]
+    path2 = random.choice(paths)
+    img1 = cv2.imread(path1, cv2.IMREAD_GRAYSCALE)
+    img2= cv2.imread(path2, cv2.IMREAD_GRAYSCALE)
+    results = siamese.predict([img1,img2])
+    print(path1,path2,results)
